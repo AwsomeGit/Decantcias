0dc17482855bc91ff8fa8921b2de4484b575d5bb
AwsomeGit <pandasalataque@gmail.com>
2026-07-16 21:58:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -68,6 +68,7 @@ function getBrandLogo(marcaRaw) {
     "rasasi": "fotos/rasassilogo.webp",
     "asdaaf": "fotos/asdaaflogo.webp",
     "jean paul gaultier": "fotos/jeanpaulgaultier.webp",
+    "emporio armani": "fotos/emporioarmani.webp",
   };
 
   for (const key in map) {
