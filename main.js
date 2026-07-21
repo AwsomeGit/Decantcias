4c941141b575a7870893ad107c9be6ddc24f5e61
AwsomeGit <pandasalataque@gmail.com>
2026-07-21 00:25:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ function getBrandLogo(marcaRaw) {
     "french avenue": "fotos/frenchavenuelogo.webp",
     "rasasi": "fotos/rasassilogo.webp",
     "asdaaf": "fotos/asdaaflogo.webp",
-    "Otros": "fotos/otrosarabeslogo.webp",
+    "otros": "fotos/otrosarabeslogo.webp",
     "jean paul gaultier": "fotos/jeanpaulgaultier.webp",
     "armani": "fotos/armani.webp", 
 
